1e1aba27e6451eea25459d7c5fa95e08937aef14
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-09-05 10:58:45 +0500

    Update script.js

--- Task-1SideBar/script.js
+++ Task-1SideBar/script.js
@@ -1,16 +1,24 @@
 const sidebar = document.getElementById("sidebar");
+
 const toggleBtn = document.getElementById("toggleBtn");
+
 const toggleIcon = toggleBtn.querySelector("i");
 
 toggleBtn.addEventListener("click", () => {
+  
   sidebar.classList.toggle("active");
 
-  // Change icon
+
   if (sidebar.classList.contains("active")) {
+    
     toggleIcon.classList.remove("fa-bars");
-    toggleIcon.classList.add("fa-times"); // X icon
-  } else {
+    
+    toggleIcon.classList.add("fa-times"); 
+  } 
+  else {
     toggleIcon.classList.remove("fa-times");
-    toggleIcon.classList.add("fa-bars"); // Hamburger icon
+    
+    toggleIcon.classList.add("fa-bars"); 
   }
 });
+
