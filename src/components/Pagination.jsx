80bc558e27b373a5582cde044f209412a3393148
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-09-06 10:21:36 +0500

    Update Pagination.jsx

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,17 +1,21 @@
 const Pagination = ({ totalPosts, postsPerPage, setCurrentPage }) => {
+  
   const pageNumbers = [];
+  
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    
     pageNumbers.push(i);
   }
 
   return (
     <div className="flex justify-center gap-2 mt-6">
+      
       {pageNumbers.map((num) => (
+      
         <button
           key={num}
           onClick={() => setCurrentPage(num)}
-          className="px-4 py-2 border rounded-lg hover:bg-blue-500 hover:text-white transition"
-        >
+          className="px-4 py-2 border rounded-lg hover:bg-blue-500 hover:text-white transition">
           {num}
         </button>
       ))}
@@ -20,3 +24,4 @@ const Pagination = ({ totalPosts, postsPerPage, setCurrentPage }) => {
 };
 
 export default Pagination;
+
