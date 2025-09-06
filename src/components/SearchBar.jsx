b96ff869c4987ab3fa9167d08b1e9339bf328e17
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-09-06 10:22:42 +0500

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,26 @@
 const SearchBar = ({ setSearchQuery }) => {
+  
   return (
+    
     <div className="flex justify-center mt-6">
-      <input
-        type="text"
-        placeholder="Search blog posts..."
+      
+      <input type="text" placeholder="Search blog posts..."
         onChange={(e) => setSearchQuery(e.target.value)}
-        className="w-full md:w-1/2 px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
-      />
+        className="w-full md:w-1/2 px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400" />
+    
     </div>
   );
 };
 
 export default SearchBar;
+
+
+
+
+
+
+
+
+
+
+
